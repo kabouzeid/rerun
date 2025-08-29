61ec8050ce57a7a7ebf4aa9157a44c6734fd5292
Andreas Reich <r_andreas2@web.de>
2025-08-29 10:57:49 +0200

    New import from URL dialog Does the same as the command pallete but is more discoverable!

--- crates/viewer/re_viewer/src/open_url.rs
+++ crates/viewer/re_viewer/src/open_url.rs
@@ -238,7 +238,7 @@ impl ViewerImportUrl {
     }
 
     /// Describes what happens when calling [`Self::open`] with this URL.
-    fn open_description(&self) -> String {
+    pub fn open_description(&self) -> String {
         match self {
             Self::IntraRecordingSelection(_) => "Go to selection".to_owned(),
 
